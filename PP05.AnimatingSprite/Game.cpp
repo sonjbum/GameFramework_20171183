06e3ea9da774adbe10f6ec8064b3eada7639c8cc
jb pc <jb pc@DESKTOP-PCQ0KUT>
2018-10-15 18:49:58 +0900

    PP05.AnimatingSprite

---
+++ PP05.AnimatingSprite/Game.cpp
@@ -0,0 +1,79 @@
+#include "Game.h"
+#include<iostream>
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRender = 0;
+
+bool g_bRunning = false;
+
+bool Game::init(const char*title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
+
+		if (g_pWindow != 0) {
+			m_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		}
+
+		m_bRunning = true;
+
+		//SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+		SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
+		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+		SDL_FreeSurface(pTempSurface);
+
+		m_sourceRectangle.w = 128;
+		m_sourceRectangle.h = 82;
+
+		m_destinationRectangle.x = m_sourceRectangle.x = 0;
+		m_destinationRectangle.y = m_sourceRectangle.y = 0;
+		m_destinationRectangle.w = m_sourceRectangle.w;
+		m_destinationRectangle.h = m_sourceRectangle.h;
+
+		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+	}
+	else {
+		return false;
+	}
+	return true;
+}
+
+void Game::render()
+{
+	SDL_RenderClear(m_pRenderer);
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+	SDL_RenderPresent(m_pRenderer);
+
+}
+
+void Game::clean()
+{
+	std::cout << "cleaning gmae\n";
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
+
+void Game::update()
+{
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+	if (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			m_bRunning = false;
+			break;
+		default:
+			break;
+
+		}
+	}
+}
+
