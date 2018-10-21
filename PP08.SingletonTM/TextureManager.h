7abce3487091c755f62d82c269cbbfd8e1b15670
jb pc <jb pc@DESKTOP-PCQ0KUT>
2018-10-21 10:51:55 +0900

    PP08.SingletonTM

---
+++ PP08.SingletonTM/TextureManager.h
@@ -0,0 +1,38 @@
+#include "SDL.h"
+#include <iostream>
+#include <map>
+
+class TextureManager
+{
+public:
+	std::map<std::string, SDL_Texture*> m_textureMap;
+
+	bool load(std::string fileName, std::string id,
+		SDL_Renderer* pRenderer);
+
+	void draw(std::string id, int x, int y, int width, int height,
+		SDL_Renderer* pRenderer,
+		SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	void drawFrame(std::string id, int x, int y, int width, int height,
+		int currentRow, int currentFrame,
+		SDL_Renderer* pRenderer,
+		SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	static TextureManager* Instance()
+	{
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new TextureManager();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+
+	TextureManager() {}
+
+private:
+	static TextureManager* s_pInstance;
+};
+
+typedef TextureManager TheTextureManager;
